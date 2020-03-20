3dea0a472f2ee43aa1b1892efc5afe372733a784
Steven Kandow <kando1sr@gmail.com>
2020-03-20 18:45:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,8 @@ def nyc_pigeon_organizer(data)
   data_color = data[:color].to_a
   pigeon_list = {}
   data_color.each do |colour|
-    colour = mini_array
     color_name = "#{colour[0]}"
-    mini_array[1].each do |name|
+    colour[1].each do |name|
       pigeon_name = name 
       if !pigeon_list[name]
         pigeon_list[name][:color] = [color_name]
