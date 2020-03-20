03b174f6ba88a7356d39c9f5aecf8c2bf18b5353
Steven Kandow <kando1sr@gmail.com>
2020-03-20 18:32:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,5 +5,6 @@ def nyc_pigeon_organizer(data)
   data_color.each do |n|
     pigeon_names << [n][1]
     end
-  pigeon_names.flatten!
+  pigeon_names_mid = pigeon_names.flatten!
+  pidgeon_names_final = pigeon_names_mid.uniq
 end
